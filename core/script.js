fae52f025995fcc6c19faf03b4d7072c065baa84
tomegathericon <kapoor.abhishek@outlook.com>
2017-04-04 00:00:09 +0530

    Adding for Nested Functions with Switch Cases

--- core/script.js
+++ core/script.js
@@ -1,6 +1,5 @@
 var linebreak = "<br />";
 
-
 function sayHello() {
    alert("Hello World")
     
@@ -11,7 +10,7 @@ document.write(linebreak);
 document.write("Tomegathericon")
 }
 
-function variables() {
+function static_variables() {
     var a;
     var b;
 
@@ -22,5 +21,91 @@ function variables() {
 
     document.write(linebreak); 
     document.write(c);
+
+}
+
+/*function get_variables() {
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("b").value);
+    var c = a+b;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    alert("The Sum is ");
+}*/
+
+
+function get_variables() {
+    var a = parseInt(prompt("Enter the first number","0"));
+    var b = parseInt(prompt("Enter the second number","0"));
+    var c = a+b;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    alert("The Sum of the numbers entered is: " +c);
+}
+
+function math_operations() {
+
+
+function sum() {
+    var a = parseInt(prompt("Enter the first number","0"));
+    var b = parseInt(prompt("Enter the second number","0"));
+    var c = a+b;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    alert("The Sum of the numbers entered is: " +c);
+};
+
+
+function subtract() {
+    var a = parseInt(prompt("Enter the first number","0"));
+    var b = parseInt(prompt("Enter the second number","0"));
+    var c = a+b;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    alert("The Difference of the numbers entered is: " +c);
+};
+
+
+
+function multiply() {
+    var a = parseInt(prompt("Enter the first number","0"));
+    var b = parseInt(prompt("Enter the second number","0"));
+    var c = a+b;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    alert("The Multiplication of the numbers entered is: " +c);
+};
+
+
+function division() {
+    var a = parseInt(prompt("Enter the first number","0"));
+    var b = parseInt(prompt("Enter the second number","0"));
+    var c = a+b;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    alert("The Division of the numbers entered is: " +c);
+};
+
+function case_value(a) {
+
+switch(a)
+{
+case 'Sum';
+sum();
+break;
+}
+}
+document.write('<h1>Chose from the following operations: </h1>');
+document.write(linebreak);
+document.write('<input type="button" value="Sum" onclick="case_value(this.value)" />');
+document.write('<input type="button" value="Diff" onclick="case_value=this.value" />');
+document.write('<input type="button" value="Mult" onclick="case_value=this.value" />');
+document.write('<input type="button" value="Div" onclick="case_value=this.value" />');
 
 }
