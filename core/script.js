312e5420b33ea6797d5ae4d3056c1155fd08023f
tomegathericon <kapoor.abhishek@outlook.com>
2017-04-03 14:48:34 +0530

    Started Core JS

---
+++ core/script.js
@@ -0,0 +1,26 @@
+var linebreak = "<br />";
+
+
+function sayHello() {
+   alert("Hello World")
+    
+}
+
+function createHello() {
+document.write(linebreak);
+document.write("Tomegathericon")
+}
+
+function variables() {
+    var a;
+    var b;
+
+    a = 1;
+    b = 2;
+
+    var c = a+b;
+
+    document.write(linebreak); 
+    document.write(c);
+
+}
