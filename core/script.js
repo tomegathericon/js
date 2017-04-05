0d783dd6ca34e53c8f30b52645680cfb31bed7f6
tomegathericon <kapoor.abhishek@outlook.com>
2017-04-05 19:43:58 +0530

    Added More Functions

--- core/script.js
+++ core/script.js
@@ -58,54 +58,62 @@ function sum() {
     alert("The Sum of the numbers entered is: " +c);
 };
 
-
 function subtract() {
     var a = parseInt(prompt("Enter the first number","0"));
     var b = parseInt(prompt("Enter the second number","0"));
-    var c = a+b;
+    var c = a-b;
     console.log(a);
     console.log(b);
     console.log(c);
     alert("The Difference of the numbers entered is: " +c);
 };
 
-
-
 function multiply() {
     var a = parseInt(prompt("Enter the first number","0"));
     var b = parseInt(prompt("Enter the second number","0"));
-    var c = a+b;
+    var c = a*b;
     console.log(a);
     console.log(b);
     console.log(c);
     alert("The Multiplication of the numbers entered is: " +c);
 };
 
-
 function division() {
     var a = parseInt(prompt("Enter the first number","0"));
     var b = parseInt(prompt("Enter the second number","0"));
-    var c = a+b;
+    var c = a/b;
     console.log(a);
     console.log(b);
     console.log(c);
     alert("The Division of the numbers entered is: " +c);
 };
 
-function case_value(a) {
-
+math_operations.case_value = function case_value(a) {
 switch(a)
 {
-case 'Sum';
+case 'Sum': 
 sum();
+break;
+
+case 'Diff' :
+subtract();
+break;
+
+case 'Mult' :
+multiply();
+break;
+
+case 'Div' :
+division();
 break;
 }
 }
+
 document.write('<h1>Chose from the following operations: </h1>');
 document.write(linebreak);
-document.write('<input type="button" value="Sum" onclick="case_value(this.value)" />');
-document.write('<input type="button" value="Diff" onclick="case_value=this.value" />');
-document.write('<input type="button" value="Mult" onclick="case_value=this.value" />');
-document.write('<input type="button" value="Div" onclick="case_value=this.value" />');
+document.write('<input type="button" value="Sum" onclick="math_operations.case_value(this.value)" />');
+document.write('<input type="button" value="Diff" onclick="math_operations.case_value(this.value)" />');
+document.write('<input type="button" value="Mult" onclick="math_operations.case_value(this.value)" />');
+document.write('<input type="button" value="Div" onclick="math_operations.case_value(this.value)" />');
 
 }
