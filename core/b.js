e0fe983af8674b4b56a643b22040f4330c437d13
tomegathericon <kapoor.abhishek@outlook.com>
2017-04-10 21:22:58 +0530

    JSON Parse Sucess

--- core/b.js
+++ core/b.js
@@ -34,13 +34,34 @@ function api_call()
 
 {
     var api = new XMLHttpRequest();
-    api.open("POST", "http://vsolutionsurvey.online/api/auth", false);
+    api.open("POST", "http://vs.impressicocrm.com/api/auth", false);
     api.setRequestHeader("Content-type", "application/json");
-    api.send(JSON.stringify({"username":prompt("Enter Your UserName"), "password":prompt("Enter Your Password")}));
+    // api.send(JSON.stringify({"username":prompt("Enter Your UserName"), "password":prompt("Enter Your Password")}));
+    api.send(JSON.stringify({"username":"admin@vsolution.com", "password":"password@111"}));
 
     console.log(api.status);
     console.log(api.statusText);
     console.log(api.readyState);
     console.log(api.responseText);
 
+    api_call.response = JSON.parse(api.responseText);
+    document.write(api_call.response.token);
 }
+
+
+function gauth()
+
+{
+    var gauth_uri = "https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/drive.metadata.readonly&include_granted_scopes=true&state=state_parameter_passthrough_value&response_type=token&client_id=901156217549-72g3d7ni931uickq04nln2gp6729q984.apps.googleusercontent.com&redirect_uri=http://jenkins.tomegathericon.host"
+    var gauth = new XMLHttpRequest();
+    gauth.open("GET", gauth_uri, false);
+    gauth.send();
+
+    console.log(gauth.status);
+    console.log(gauth.statusText);
+    console.log(gauth.readyState);
+    console.log(gauth.responseText);
+}
+
+
+
