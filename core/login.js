0590f51d1ff4025661c627eaa934342b84a9c5d4
tomegathericon <kapoor.abhishek@outlook.com>
2017-04-11 16:51:08 +0530

    Created Login

---
+++ core/login.js
@@ -0,0 +1,74 @@
+
+var linebreak = "<br />";
+var base_url = "http://vs.impressicocrm.com";
+//var base_url = "http://vsolutionsurvey.online";
+
+
+function login()
+
+{
+    login.api = function ()
+    {
+        var api = new XMLHttpRequest();
+        api.open("POST", base_url+"/api/auth", true);
+        api.setRequestHeader("Content-type", "application/json");
+        api.send(JSON.stringify({"username":login_form.username.value, "password":login_form.password.value}));
+        api.onreadystatechange = function () 
+          {
+            if (this.readyState == 4 && this.status == 200)
+            {
+
+
+        console.log(api.status);
+        console.log(api.statusText);
+        console.log(api.readyState);
+        console.log(api.responseText);
+            }
+          }
+    }
+
+    
+    login_form = document.createElement("form");
+    login_form.id = "login_form";
+        
+    username = document.createElement('input');
+    username.id = "username";
+    username.type = "textbox";
+    username.value = "admin@vsolution.com";
+
+    password = document.createElement('input');
+    password.id = "password";
+    password.type = "password";
+    password.value = "password@111";
+
+    submit = document.createElement('input');
+    submit.id = "submit";
+    submit.type = "button";
+    submit.value = "Login";
+    submit.onclick=function(){login.api();};
+
+    reset = document.createElement('input');
+    reset.id = "reset";
+    reset.type = "reset";
+    reset.value ="Reset";
+
+    usernametext = document.createTextNode("Username:");
+    passwordtext = document.createTextNode("Password:");
+    nextline = document.createElement("br");
+
+    login_form.appendChild(usernametext);
+    login_form.appendChild(username);
+    login_form.appendChild(nextline);
+    login_form.appendChild(passwordtext);
+    login_form.appendChild(password);
+    nextline = document.createElement("br");
+    login_form.appendChild(nextline);
+    login_form.appendChild(submit);
+    login_form.appendChild(reset);
+    
+    
+    document.body.appendChild(login_form); 
+
+
+}
+
