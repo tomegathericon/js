f9c4055623b207b0801057f5e8fb29e025342722
tomegathericon <kapoor.abhishek@outlook.com>
2017-04-12 23:36:57 +0530

    Get Region + Get Survey

--- core/home.js
+++ core/home.js
@@ -2,12 +2,33 @@ login_response = JSON.parse(window.localStorage.getItem("login_response"))
 var base_url = "http://vs.impressicocrm.com";
 document.write("Hello "+login_response.user.first_name+" "+login_response.user.last_name);
 
-function get_surveys()
+function get_regions()
+{   
+    regions_text = document.createTextNode("Please Chose From The Following Regions:");
+    nextline = document.createElement("br");
+    document.body.appendChild(nextline);
+    document.body.appendChild(regions_text);
+
+    for ( f=0; f < login_response.user.regions.length; f++ )
+    {
+        regions = document.createElement("input");
+        regions.type = "button";
+        regions.value = login_response.user.regions[f].region_title;
+        regions.id = login_response.user.regions[f].region_id
+        regions.onclick = function () {get_surveys(this.id);};
+        nextline = document.createElement("br");
+        console.log(regions);
+        document.body.appendChild(nextline);
+        document.body.appendChild(regions);
+    }
+}
+function get_surveys(regions_id)
 
 {
          
         var api = new XMLHttpRequest();
-        var params = "with=question&su_region_ids=1&su_region_ids=3"; 
+        alert(regions_id);
+        var params = "with=question&su_region_ids="+regions_id+"&su_region_ids="+regions_id; 
         api.open("GET", base_url+"/api/survey?"+params, true);
         api.setRequestHeader("Content-type", "application/json");
         api.setRequestHeader("Authorization", "Bearer "+login_response.token);
@@ -25,12 +46,14 @@ function get_surveys()
                 
             }
 
-            if (this.status == 400)
+            if (this.readyState ==4 && this.status != 200)
             {
                 console.log(this.status);
                 console.log(this.statusText);
                 console.log(this.readyState);
                 console.log(this.responseText);
+                alert("Uh-Oh");
+                location.reload();
             }
              
            // {
