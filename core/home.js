80caccf22efe4103cebf8172f6bffd5b12f63274
tomegathericon <kapoor.abhishek@outlook.com>
2017-04-16 09:41:23 +0530

    Init Angular

--- core/home.js
+++ core/home.js
@@ -4,7 +4,7 @@ var base_url = "http://vs.impressicocrm.com";
 function greeting()
 {
     var date = new Date();
-    
+
     if ( date.getHours() >= 0 && date.getHours() < 8 )
     {
         document.write("You Should Be Sleeping "+login_response.user.first_name+" "+login_response.user.last_name);
@@ -50,22 +50,22 @@ function get_logged_in_user_profile()
     user_profile = {};
     user_profile.show = new Array();
     user_profile.show.push(
-        
+
         {"Gender"     : login_response.user.gender},
         {"First Name" : login_response.user.first_name},
         {"Last Name"  : login_response.user.last_name},
         {"Email"      : login_response.user.email},
         {"Phone"      : login_response.user.phone1},
         {"Alternate Phone" : login_response.user.phone2}
-        
+
     );
     user_table = document.createElement("table");
     user_table_caption = document.createElement("caption");
     user_table_caption.innerHTML = "<h2>User Profile</h2>";
     user_table.appendChild(user_table_caption);
     user_table_style = document.createElement("style");
-    user_table_style.innerHTML = "table, tr, td { border: 1px solid black; border-collapse: collapse; padding: 15px; }" 
-    
+    user_table_style.innerHTML = "table, tr, td { border: 1px solid black; border-collapse: collapse; padding: 15px; }"
+
     for ( f=0; f<user_profile.show.length; f++ )
     {
     row = document.createElement("tr");
@@ -79,20 +79,20 @@ function get_logged_in_user_profile()
     user_table.appendChild(row);
     }
 
-    document.body.innerHTML=""; 
+    document.body.innerHTML="";
     document.body.appendChild(user_table);
     document.body.appendChild(user_table_style);
-    
+
 }
- 
+
 
 function get_regions()
-{   
+{
     regions_text = document.createTextNode("Please Chose From The Following Regions To Get The Corresponding Surveys:");
     nextline = document.createElement("br");
     document.body.innerHTML = "";
     document.body.appendChild(regions_text);
-    
+
 
     for ( f=0; f < login_response.user.regions.length; f++ )
     {
@@ -110,17 +110,17 @@ function get_regions()
 function get_surveys(regions_id)
 
 {
-         
+
         var api = new XMLHttpRequest();
         alert(regions_id);
-        var params = "with=question&su_region_ids="+regions_id+"&su_region_ids="+regions_id; 
+        var params = "with=question&su_region_ids="+regions_id+"&su_region_ids="+regions_id;
         api.open("GET", base_url+"/api/survey?"+params, true);
         api.setRequestHeader("Content-type", "application/json");
         api.setRequestHeader("Authorization", "Bearer "+login_response.token);
         api.send();
-        api.onreadystatechange = function () 
-          { 
-            
+        api.onreadystatechange = function ()
+          {
+
             if (this.readyState == 4 && this.status == 200)
             {
                // alert("IF");
@@ -128,8 +128,8 @@ function get_surveys(regions_id)
                 console.log(api.statusText);
                 console.log(api.readyState);
                 console.log(api.responseText);
-              
-                
+
+
             }
 
             if (this.readyState ==4 && this.status != 200)
